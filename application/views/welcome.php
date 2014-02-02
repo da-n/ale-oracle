c900fbbc582ea440a55c0421017da01a014faaf8
Daniel Davidson <dan@bitmono.com>
2014-02-02 23:07:57 +0000

    Added slider loop

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -2,113 +2,21 @@
       <section class="welcome">
         <h3>&ldquo;Ale Oracle, help me!&rdquo;</h3>
       </section>
+      <?php if ($sliders) : ?>
       <section>
         <ul id="scroller">
+          <?php foreach ($sliders as $slider) : ?>
             <li>
-              <img src="/assets/img/profiles/profile-01.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
+              <img src="/assets/img/profiles/thumbs/profile-<?php echo $slider['id']; ?>.jpg" width="40" height="40" class="thumb">
+              <img src="/assets/img/ales/thumbs/ale-<?php echo $slider['ale']; ?>.jpg" width="40" height="40" class="thumb">
+              <h5><?php echo $slider['ale_name']; ?></h5>
             </li>
-            <li>
-              <img src="/assets/img/profiles/profile-02.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-03.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-04.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-05.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-06.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-07.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-08.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-09.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-10.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-11.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-12.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-13.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-14.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-15.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-16.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-17.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-18.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-19.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
-            <li>
-              <img src="/assets/img/profiles/profile-20.jpg" width="40" height="40" class="thumb">
-              <img src="http://lorempixel.com/40/40/" width="40" height="40" class="thumb">
-              <h5>Late Red</h5>
-            </li>
+          <?php endforeach; ?>
         </ul>
-        </section>
-        <section class="next">
-          <h5>Answer 3 simple questions and the Ale Oracle will recommend a matching ale.</h5>
-          <a href="#" class="button">Consult The Ale Oracle <span class="chevron right"></span></a>
-        </section>
+      </section>
+      <?php endif; ?>
+      <section class="next">
+        <h5>Answer 3 simple questions and the Ale Oracle will recommend a matching ale.</h5>
+        <a href="#" class="button">Consult The Ale Oracle <span class="chevron right"></span></a>
       </section>
     </article>
